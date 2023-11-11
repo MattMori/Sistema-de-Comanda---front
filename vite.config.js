ab51e9dcfd5cb119d3c2ced07dc8eb9366510d31
MattMori <mateus5310@live.com>
2023-11-10 22:58:35 -0300

    Correção de roteamento

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base: '/',
   server: {
     proxy: {
@@ -13,8 +12,3 @@ export default defineConfig({
   build: {
   },
 });
-=======
-  base: '/'
-})
-
->>>>>>> 7e2cb2f9a4491c0258813008e6c28c6d54594af9
