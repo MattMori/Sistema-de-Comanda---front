532b1641c882e61c4251e9e716a03f027b62bebf
MattMori <mateus5310@live.com>
2023-11-16 12:01:50 -0300

    Troca do Alert pelo sweetalert2 na tela home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import { SistemaService } from '../../api/sistemaService';
+import Swal from 'sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 import "./index.scss";
 
 const Home = () => {
-
   const [comanda, setComanda] = useState({
     cpf: "",
     numeroDaComanda: ''
@@ -26,51 +27,65 @@ const Home = () => {
         cpf: "",
         numeroDaComanda: ''
       });
-      alert('Comanda Associada com Sucesso')
-    } catch (error) {
-      console.error("Erro ao cadastrar o cliente:", error);
-    }
 
+      // Exibe notificação de sucesso
+      Swal.fire({
+        icon: 'success',
+        title: 'Comanda associada com sucesso!',
+        showConfirmButton: false,
+        timer: 1500
+      });
+
+    } catch (error) {
+      console.error("Erro ao cadastrar a comanda:", error);
+
+      // Exibe notificação de erro
+      Swal.fire({
+        icon: 'error',
+        title: 'Erro ao associar a comanda',
+        text: 'Por favor, tente novamente.'
+      });
+    }
   };
 
   return (
     <div className="home">
-    <div className="home-container">
-      <h1>Bem-vindo ao BarSystem</h1><br />
-      <p> Cadastre as comandas aqui</p>
-      <div className="cadastro-container">
-        <form onSubmit={handleSubmit}>
-          <label>
-            CPF:
+      <div className="home-container">
+        <h1>Bem-vindo ao BarSystem</h1>
+        <p>Cadastre as comandas aqui</p>
+        <div className="cadastro-container">
+          <form onSubmit={handleSubmit}>
+            <label>
+              CPF:
+              <br />
+              <input
+                type="text"
+                name="cpf"
+                placeholder="Digite o CPF do cliente aqui"
+                value={comanda.cpf}
+                onChange={handleChange}
+                required
+              />
+            </label>
+            <br />
+            <label>
+              Numero Da Comanda:
+              <br />
+              <input
+                type="text"
+                name="numeroDaComanda"
+                placeholder="Digite o Numero da comanda aqui"
+                value={comanda.numeroDaComanda}
+                onChange={handleChange}
+                required
+              />
+            </label>
             <br />
 
-            <input
-              type="text"
-              name="cpf"
-              placeholder="Digite o CPF do cliente aqui"
-              value={comanda.cpf}
-              onChange={handleChange}
-              required
-            />
-          </label>
-          <br />
-          <label>
-            Numero Da Comanda:
-            <br />
-            <input
-              type="text"
-              name="numeroDaComanda"
-              placeholder="Digite o Numero da comanda aqui"
-              value={comanda.numeroDaComanda}
-              onChange={handleChange}
-              required
-            />{" "}
-          </label><br />
-
-          <button type="submit">Cadastrar</button>
-        </form>
+            <button type="submit">Cadastrar</button>
+          </form>
+        </div>
       </div>
-    </div>
     </div>
   );
 };
